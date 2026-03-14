61ff82074890b1aca93cfef0456236bad3a12676
Aviral00Jain <jainaviral183@gmail.com>
2026-03-14 14:11:49 +0530

    [Aviral]: modified a program of feet test4

--- src/test/java/com/apps/quantitymeasurement/domain/FeetTest.java
+++ src/test/java/com/apps/quantitymeasurement/domain/FeetTest.java
@@ -1,51 +1,124 @@
 package com.apps.quantitymeasurement.domain;
 
-import org.junit.jupiter.api.Test;
+import quantitymeasurement.controller.QuantityMeasurementController;
+import quantitymeasurement.entity.QuantityDTO;
+import quantitymeasurement.exception.QuantityMeasurementException;
+import quantitymeasurement.repository.IQuantityMeasurementRepository;
+import quantitymeasurement.repository.QuantityMeasurementCacheRepository;
+import quantitymeasurement.service.IQuantityMeasurementService;
+import quantitymeasurement.service.QuantityMeasurementServiceImpl;
 
-import quantitymeasurement.domain.Feet;
-import quantitymeasurement.domain.InvalidFeetException;
+public class QuantityMeasurementAppTest {
+	private static IQuantityMeasurementRepository repository;
+	private static IQuantityMeasurementService service;
+	private static QuantityMeasurementController controller;
+	
+	@BeforeAll
+	public static void intialize() {
+		repository = QuantityMeasurementCacheRepository.getInstance();
+		service = new QuantityMeasurementServiceImpl(repository);
+        controller = new QuantityMeasurementController(service);
+	}
+	
+	@Test
+	public void addLengthFeetAndInches() {
+	    QuantityDTO feet = new QuantityDTO(1.0, QuantityDTO.LengthUnit.FEET);
+	    QuantityDTO inches = new QuantityDTO(12.0, QuantityDTO.LengthUnit.INCHES);
 
-import static org.junit.jupiter.api.Assertions.*;
+	    QuantityDTO result = controller.performAdd(feet, inches);
 
-public class FeetTest {
+	    assertEquals(2.0, result.getValue());
+	}
 
-    private final Feet sample = Feet.fromString("1.0"); // create once using fromString
+	@Test
+	public void subtractLengthFeetAndInches() {
+	    QuantityDTO feet = new QuantityDTO(5.0, QuantityDTO.LengthUnit.FEET);
+	    QuantityDTO inches = new QuantityDTO(12.0, QuantityDTO.LengthUnit.INCHES);
 
-    @Test
-    void givenSameValueWhenComparedThenShouldBeEqual() {
-        Feet other = Feet.fromString("1.0");
-        assertEquals(sample, other);
-    }
+	    QuantityDTO result = controller.performSubtract(feet, inches);
 
-    @Test
-    void givenDifferentValueWhenComparedThenShouldNotBeEqual() {
-        Feet other = Feet.fromString("2.0");
-        assertNotEquals(sample, other);
-    }
+	    assertEquals(4.0, result.getValue());
+	}
 
-    @Test
-    void givenNullWhenComparedThenShouldReturnFalse() {
-        assertNotEquals(null, sample);
-    }
+	@Test
+	public void divideLengthFeet() {
+	    QuantityDTO length1 = new QuantityDTO(10.0, QuantityDTO.LengthUnit.FEET);
+	    QuantityDTO length2 = new QuantityDTO(5.0, QuantityDTO.LengthUnit.FEET);
 
-    @Test
-    void givenSameReferenceWhenComparedThenShouldReturnTrue() {
-        assertEquals(sample, sample);
-    }
+	    double result = controller.performDivide(length1, length2);
 
-    @Test
-    void givenDifferentTypeWhenComparedThenShouldReturnFalse() {
-        assertFalse(sample.equals("1.0"));
-    }
+	    assertEquals(2.0, result);
+	}
 
-    @Test
-    void givenValidStringInputThenShouldCreateFeet() {
-        Feet fromString = Feet.fromString("1.0");
-        assertEquals(sample, fromString);
-    }
+	@Test
+	public void addWeightKilogramsAndGrams() {
+	    QuantityDTO kg = new QuantityDTO(1.0, QuantityDTO.WeightUnit.KILOGRAM);
+	    QuantityDTO grams = new QuantityDTO(500.0, QuantityDTO.WeightUnit.GRAM);
 
-    @Test
-    void givenInvalidStringInputThenShouldThrowException() {
-        assertThrows(InvalidFeetException.class, () -> Feet.fromString("abc"));
-    }
+	    QuantityDTO result = controller.performAdd(kg, grams);
+
+	    assertEquals(1.5, result.getValue());
+	}
+
+	@Test
+	public void subtractWeightKilogramsAndGrams() {
+	    QuantityDTO kg = new QuantityDTO(2.0, QuantityDTO.WeightUnit.KILOGRAM);
+	    QuantityDTO grams = new QuantityDTO(500.0, QuantityDTO.WeightUnit.GRAM);
+
+	    QuantityDTO result = controller.performSubtract(kg, grams);
+
+	    assertEquals(1.5, result.getValue());
+	}
+
+	@Test
+	public void addVolumeLitersAndMillilitres() {
+	    QuantityDTO liter = new QuantityDTO(1.0, QuantityDTO.VolumeUnit.LITRE);
+	    QuantityDTO ml = new QuantityDTO(500.0, QuantityDTO.VolumeUnit.MILLILITRE);
+
+	    QuantityDTO result = controller.performAdd(liter, ml);
+
+	    assertEquals(1.5, result.getValue());
+	}
+
+	@Test
+	public void convertVolumeLitersToMillilitres() {
+	    QuantityDTO liter = new QuantityDTO(1.0, QuantityDTO.VolumeUnit.LITRE);
+	    QuantityDTO ml = new QuantityDTO(0, QuantityDTO.VolumeUnit.MILLILITRE);
+
+	    QuantityDTO result = controller.performConvert(liter, "MILLILITRE");
+
+	    assertEquals(1000.0, result.getValue());
+	}
+
+	@Test
+	public void preventLengthWeightAddition() {
+	    QuantityDTO length = new QuantityDTO(1.0, QuantityDTO.LengthUnit.FEET);
+	    QuantityDTO weight = new QuantityDTO(1.0, QuantityDTO.WeightUnit.KILOGRAM);
+
+	    assertThrows(QuantityMeasurementException.class,
+	            () -> controller.performAdd(length, weight)
+	    );
+	}
+
+	@Test
+	public void preventLengthVolumeComparison() {
+	    QuantityDTO length = new QuantityDTO(1.0, QuantityDTO.LengthUnit.FEET);
+	    QuantityDTO volume = new QuantityDTO(1.0, QuantityDTO.VolumeUnit.LITRE);
+
+	    assertThrows(
+	            QuantityMeasurementException.class,
+	            () -> controller.performCompare(length, volume)
+	    );
+	}
+
+	@Test
+	public void preventDivisionByZero() {
+	    QuantityDTO length1 = new QuantityDTO(10.0, QuantityDTO.LengthUnit.FEET);
+	    QuantityDTO zeroLength = new QuantityDTO(0.0, QuantityDTO.LengthUnit.FEET);
+
+	    assertThrows(
+	            QuantityMeasurementException.class,
+	            () -> controller.performDivide(length1, zeroLength)
+	    );
+	}
 }
