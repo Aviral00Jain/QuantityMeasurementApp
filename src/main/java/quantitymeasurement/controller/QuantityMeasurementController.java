8ecc8852d062fcadb2cd65d66520b15247ba58b9
Aviral00Jain <jainaviral183@gmail.com>
2026-03-14 14:44:18 +0530

    [Aviral]:modified a program of controller

--- src/main/java/quantitymeasurement/controller/QuantityMeasurementController.java
+++ src/main/java/quantitymeasurement/controller/QuantityMeasurementController.java
@@ -1,5 +1,43 @@
 package quantitymeasurement.controller;
 
-public class QuantityMeasurementController {
+import quantitymeasurement.entity.QuantityDTO;
+import quantitymeasurement.exception.QuantityMeasurementException;
+import quantitymeasurement.service.IQuantityMeasurementService;
 
+public class QuantityMeasurementController {
+    private IQuantityMeasurementService service;
+
+    public QuantityMeasurementController(IQuantityMeasurementService service) {
+        this.service = service;
+    }
+
+    public boolean performCompare(QuantityDTO q1, QuantityDTO q2) {
+    	boolean result = service.compare(q1, q2);
+        System.out.println("Comparison Result: " + (result ? "Equal" : "Not Equal"));
+        return result;
+    }
+
+    public QuantityDTO performConvert(QuantityDTO quantity, String targetUnit) {
+    	QuantityDTO result = service.convert(quantity, targetUnit);
+        System.out.println("Converted Result: " + result.getValue() + " " + result.getUnit().getUnitName());
+        return result;
+    }
+
+    public QuantityDTO performAdd(QuantityDTO q1, QuantityDTO q2) {
+    	QuantityDTO result = service.add(q1, q2);
+        System.out.println("Addition Result: " + result.getValue() + " " + result.getUnit().getUnitName());
+        return result;
+    }
+
+    public QuantityDTO performSubtract(QuantityDTO q1, QuantityDTO q2) {
+    	QuantityDTO result = service.subtract(q1, q2);
+        System.out.println("Subtraction Result: " + result.getValue() + " " + result.getUnit().getUnitName());
+        return result;
+    }
+
+    public double performDivide(QuantityDTO q1, QuantityDTO q2) {
+    	double result = service.divide(q1, q2);
+        System.out.println("Division Result: " + result);
+        return result;
+    }
 }
