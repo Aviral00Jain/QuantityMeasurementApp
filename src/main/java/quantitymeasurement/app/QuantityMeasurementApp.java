7b32649a722e6a2a2dc427cf8af2efe2679fccd7
Aviral00Jain <jainaviral183@gmail.com>
2026-03-18 11:36:45 +0530

    [Aviral]: modified a program of quantityMeasurementApp

--- src/main/java/quantitymeasurement/app/QuantityMeasurementApp.java
+++ src/main/java/quantitymeasurement/app/QuantityMeasurementApp.java
@@ -1,26 +1,42 @@
 package quantitymeasurement.app;
 
+import quantitymeasurement.controller.QuantityMeasurementController;
+import quantitymeasurement.entity.QuantityDTO;
+import quantitymeasurement.repository.IQuantityMeasurementRepository;
+import quantitymeasurement.repository.QuantityMeasurementCacheRepository;
+import quantitymeasurement.repository.QuantityMeasurementDatabaseRepository;
+import quantitymeasurement.service.IQuantityMeasurementService;
+import quantitymeasurement.service.QuantityMeasurementServiceImpl;
 
-import quantitymeasurement.domain.LengthUnit;
-import quantitymeasurement.domain.Quantity;
-import quantitymeasurement.domain.TemperatureUnit;
-import quantitymeasurement.domain.VolumeUnit;
-import quantitymeasurement.domain.WeightUnit;
 public class QuantityMeasurementApp {
-	
-	public static void main(String[] args) {
-		
-        System.out.println( new Quantity<>(0.0, TemperatureUnit.CELSIUS).equals(new Quantity<>(32.0, TemperatureUnit.FAHRENHEIT)));
 
-        System.out.println( new Quantity<>(100.0, TemperatureUnit.CELSIUS).convertTo(TemperatureUnit.FAHRENHEIT));
+    public static void main(String[] args) {
+    	IQuantityMeasurementRepository repository = QuantityMeasurementDatabaseRepository.getInstance();
 
-        try {
-            new Quantity<>(100.0, TemperatureUnit.CELSIUS).add(new Quantity<>(50.0, TemperatureUnit.CELSIUS));
-        
-        } catch (Exception e) {
-        	
-            System.out.println(e.getMessage());
-        }
-        
-	}
+        IQuantityMeasurementService service = new QuantityMeasurementServiceImpl(repository);
+
+        QuantityMeasurementController controller = new QuantityMeasurementController(service);
+
+        QuantityDTO length1 = new QuantityDTO(10, QuantityDTO.LengthUnit.FEET);
+        QuantityDTO length2 = new QuantityDTO(120, QuantityDTO.LengthUnit.INCHES);
+        controller.performCompare(length1, length2);
+
+        QuantityDTO weight = new QuantityDTO(1, QuantityDTO.WeightUnit.KILOGRAM);
+        controller.performConvert(weight, "GRAM");
+
+        QuantityDTO feet = new QuantityDTO(10, QuantityDTO.LengthUnit.FEET);
+        QuantityDTO inches = new QuantityDTO(6, QuantityDTO.LengthUnit.INCHES);
+        controller.performAdd(feet, inches);
+
+        controller.performSubtract(feet, inches);
+
+        QuantityDTO feet20 = new QuantityDTO(20, QuantityDTO.LengthUnit.FEET);
+        QuantityDTO feet5 = new QuantityDTO(5, QuantityDTO.LengthUnit.FEET);
+        controller.performDivide(feet20, feet5);
+
+        QuantityDTO temp1 = new QuantityDTO(0, QuantityDTO.TemperatureUnit.CELSIUS);
+        QuantityDTO temp2 = new QuantityDTO(32, QuantityDTO.TemperatureUnit.FAHRENHEIT);
+
+        controller.performCompare(temp1, temp2);
+    }
 }
