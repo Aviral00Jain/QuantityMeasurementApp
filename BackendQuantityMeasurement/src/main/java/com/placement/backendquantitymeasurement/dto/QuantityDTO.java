b8ef1cabdd02b922dbc9e52e0e0ea1f49d8a9b06
Aviral00Jain <jainaviral183@gmail.com>
2026-03-24 12:29:54 +0530

    [Aviral]: Added a program of QuantityDTO

--- BackendQuantityMeasurement/src/main/java/com/placement/backendquantitymeasurement/dto/QuantityDTO.java
+++ BackendQuantityMeasurement/src/main/java/com/placement/backendquantitymeasurement/dto/QuantityDTO.java
@@ -7,4 +7,16 @@ public class QuantityDTO {
     private Double value;
     private String unit;
     private String measurementType;
+	public Object getValue() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	public Object getUnit() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	public Object getMeasurementType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
