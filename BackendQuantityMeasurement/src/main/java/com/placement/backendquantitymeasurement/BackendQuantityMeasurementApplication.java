6f2b0df176f01b85b6179901944abfb314524ded
Aviral00Jain <jainaviral183@gmail.com>
2026-04-02 11:38:45 +0530

    [Aviral]: modified a application file

--- BackendQuantityMeasurement/src/main/java/com/placement/backendquantitymeasurement/BackendQuantityMeasurementApplication.java
+++ BackendQuantityMeasurement/src/main/java/com/placement/backendquantitymeasurement/BackendQuantityMeasurementApplication.java
@@ -1,4 +1,4 @@
- package com.placement.backendquantitymeasurement;
+package com.placement.backendquantitymeasurement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BackendQuantityMeasurementApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(BackendQuantityMeasurementApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(BackendQuantityMeasurementApplication.class, args);
+	}
 
 }
