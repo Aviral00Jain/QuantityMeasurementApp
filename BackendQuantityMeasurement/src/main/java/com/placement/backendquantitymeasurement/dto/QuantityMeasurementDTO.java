bd055dab493d53e0f69a3c197009040b6e598af9
Aviral00Jain <jainaviral183@gmail.com>
2026-04-02 12:02:53 +0530

    [Aviral]: modified quantitymeasurementDTO

--- BackendQuantityMeasurement/src/main/java/com/placement/backendquantitymeasurement/dto/QuantityMeasurementDTO.java
+++ BackendQuantityMeasurement/src/main/java/com/placement/backendquantitymeasurement/dto/QuantityMeasurementDTO.java
@@ -1,33 +1,101 @@
 package com.placement.backendquantitymeasurement.dto;
 
-import lombok.*;
+import lombok.Data;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.app.quantitymeasurement.model.QuantityMeasurementEntity;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class QuantityMeasurementDTO {
+    public double thisValue;
+    public String thisUnit;
+    public String thisMeasurementType;
 
-    public QuantityMeasurementDTO(Object value, Object unit, Object measurementType, Object value2, Object unit2,
-			Object measurementType2, String string, Object object, Object object2, Object object3, String valueOf,
-			Object object4, boolean b) {
-		// TODO Auto-generated constructor stub
-	}
-	private Double thisValue;
-    private String thisUnit;
-    private String thisMeasurementType;
+    public double thatValue;
+    public String thatUnit;
+    public String thatMeasurementType;
 
-    private Double thatValue;
-    private String thatUnit;
-    private String thatMeasurementType;
+    public String operation;
 
-    private String operation;
+    public String resultString;
+    public double resultValue;
+    public String resultUnit;
+    public String resultMeasurementType;
 
-    private Double resultValue;
-    private String resultUnit;
-    private String resultMeasurementType;
+    public String errorMessage;
 
-    private String resultString;
+    /**
+     * Rename field from isError -> error
+     * Jackson strips "is" prefix from boolean getters.
+     */
+    @JsonProperty("error")
+    public boolean error;
 
-    private String errorMessage;
-    private boolean error;
+    // Convert Entity to DTO
+    public static QuantityMeasurementDTO from(QuantityMeasurementEntity entity) {
+        if (entity == null) return null;
+
+        QuantityMeasurementDTO dto = new QuantityMeasurementDTO();
+
+        dto.thisValue = entity.thisValue;
+        dto.thisUnit = entity.thisUnit;
+        dto.thisMeasurementType = entity.thisMeasurementType;
+
+        dto.thatValue = entity.thatValue;
+        dto.thatUnit = entity.thatUnit;
+        dto.thatMeasurementType = entity.thatMeasurementType;
+
+        dto.operation = entity.operation;
+
+        dto.resultString = entity.resultString;
+        dto.resultValue = entity.resultValue;
+        dto.resultUnit = entity.resultUnit;
+        dto.resultMeasurementType = entity.resultMeasurementType;
+
+        dto.errorMessage = entity.errorMessage;
+        dto.error = entity.isError;
+
+        return dto;
+    }
+
+    // Convert DTO to Entity
+    public QuantityMeasurementEntity toEntity() {
+        QuantityMeasurementEntity entity = new QuantityMeasurementEntity();
+
+        entity.thisValue = this.thisValue;
+        entity.thisUnit = this.thisUnit;
+        entity.thisMeasurementType = this.thisMeasurementType;
+
+        entity.thatValue = this.thatValue;
+        entity.thatUnit = this.thatUnit;
+        entity.thatMeasurementType = this.thatMeasurementType;
+
+        entity.operation = this.operation;
+
+        entity.resultString = this.resultString;
+        entity.resultValue = this.resultValue;
+        entity.resultUnit = this.resultUnit;
+        entity.resultMeasurementType = this.resultMeasurementType;
+
+        entity.errorMessage = this.errorMessage;
+        entity.isError = this.error;
+
+        return entity;
+    }
+
+    // Convert list of entities to list of DTOs
+    public static List<QuantityMeasurementDTO> fromList(List<QuantityMeasurementEntity> entities) {
+        return entities.stream()
+                .map(QuantityMeasurementDTO::from)
+                .collect(Collectors.toList());
+    }
+
+    // Convert list of DTOs to list of entities
+    public static List<QuantityMeasurementEntity> toEntityList(List<QuantityMeasurementDTO> dtos) {
+        return dtos.stream()
+                .map(QuantityMeasurementDTO::toEntity)
+                .collect(Collectors.toList());
+    }
 }
