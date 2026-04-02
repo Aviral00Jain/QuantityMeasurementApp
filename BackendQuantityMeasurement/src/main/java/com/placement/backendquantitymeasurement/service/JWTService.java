10422efe0c0f364d0d2aba5c98f3720a9ed99dcb
Aviral00Jain <jainaviral183@gmail.com>
2026-04-02 13:52:28 +0530

    [Aviral]: added a program of service/jwtservice

---
+++ BackendQuantityMeasurement/src/main/java/com/placement/backendquantitymeasurement/service/JWTService.java
@@ -0,0 +1,10 @@
+package com.placement.backendquantitymeasurement.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface JWTService {
+    public String generateToken(String username);
+    public boolean validateToken(String token, UserDetails userDetails);
+
+    public String extractUserName(String token);
+}
